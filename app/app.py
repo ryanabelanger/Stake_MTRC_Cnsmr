62becea884b09dc6a0d9527385b34b9a00a5b8ad
Ryan Belanger <ryanabelanger@gmail.com>
2022-01-23 22:32:00 -0800

    Removing Graphite

--- app/app.py
+++ app/app.py
@@ -13,6 +13,8 @@ connection = pika.BlockingConnection(pika.ConnectionParameters('brs-exchange', c
 GrphtQueue = connection.channel()
 GrphtQueue.queue_declare(queue='Grpht_Queue', durable=True)
 
+# TODO: On/off config for Graphite. If it's off empty into the void.
+#  UPDATE: Abandoning Graphite. Sending to void implemented as "If True"
 
 def ProcessMessage(ch, method, properties, body):
     print(" [X] Received. Processing... \n")
@@ -21,10 +23,13 @@ def ProcessMessage(ch, method, properties, body):
     msg = j["Msg"]
     ts = j["Ts"]
     frmtmsg = "{} {} {}\n".format(metric, str(msg), ts).encode('utf-8')
-    sock = socket.socket()
-    sock.connect((GRPHT_S, GRPHT_P))
-    sock.sendall(frmtmsg)
-    sock.close()
+    if True:
+        print("[ VOID PROTOCOL INITIATED ] \n MSG = {}".format(frmtmsg))
+    else:
+        sock = socket.socket()
+        sock.connect((GRPHT_S, GRPHT_P))
+        sock.sendall(frmtmsg)
+        sock.close()
     print(" [X] Done \n")
     ch.basic_ack(delivery_tag=method.delivery_tag)
     print(" [X] Ack'ed. Waiting for the next message. \n")
